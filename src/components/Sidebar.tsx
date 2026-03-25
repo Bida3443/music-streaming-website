3be7d81dc7b11c749d39a0587c83c9f4824a3410
Bida3443 <alexanderbamise@gmail.com>
2026-03-25 10:19:22 -0700

    first commit

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,111 @@
+"use client"
+
 import Link from "next/link";
-import { LuPlug } from "react-icons/lu";
+import { LuPlus } from "react-icons/lu";
+import Image from "next/image";
+import { MdOutlineLibraryMusic } from "react-icons/md";
+import { useState } from "react";
 
 
  export default function Sidebar() {
+
+    const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div>
-        <aside className="fixed left-2 top-15 bg-background w-75 rounded-lg h-[90vh] p-2 overflow-y-auto">
+        <aside className={`fixed left-2 top-15 bg-background w-75 rounded-lg h-[90vh] p-2 overflow-y-auto             ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
             <div className="flex justify-between text-primary-text items-center p-2 mb-4 ">
                 <h2 className="font-bold">You Library</h2>
                 <Link href="upload-song">
-                <LuPlug/>
+                <LuPlus size="20"/>
                 </Link>
+            </div>
+            <div>
+                <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                    <Image 
+                    src="/cover-1.jpeg"
+                    alt= "cover-image"
+                    width={300}
+                    height={300}
+                    className="w-10 h-10 object-cover rounded-md"
+                     />
+                     <div>
+                        <p className="text-primary-text font-semibold">Midnigth Echoes</p>
+                        <p className="text-secondary-text text-sm">By Neon Skyline</p>
+                     </div>
+
+                </div>
+            </div>
+            <div>
+                <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                    <Image 
+                    src="/cover-1.jpeg"
+                    alt= "cover-image"
+                    width={300}
+                    height={300}
+                    className="w-10 h-10 object-cover rounded-md"
+                     />
+                     <div>
+                        <p className="text-primary-text font-semibold">Midnigth Echoes</p>
+                        <p className="text-secondary-text text-sm">By Neon Skyline</p>
+                     </div>
+
+                </div>
+            </div>
+            <div>
+                <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                    <Image 
+                    src="/cover-1.jpeg"
+                    alt= "cover-image"
+                    width={300}
+                    height={300}
+                    className="w-10 h-10 object-cover rounded-md"
+                     />
+                     <div>
+                        <p className="text-primary-text font-semibold">Midnigth Echoes</p>
+                        <p className="text-secondary-text text-sm">By Neon Skyline</p>
+                     </div>
+
+                </div>
+            </div>
+            <div>
+                <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                    <Image 
+                    src="/cover-1.jpeg"
+                    alt= "cover-image"
+                    width={300}
+                    height={300}
+                    className="w-10 h-10 object-cover rounded-md"
+                     />
+                     <div>
+                        <p className="text-primary-text font-semibold">Midnigth Echoes</p>
+                        <p className="text-secondary-text text-sm">By Neon Skyline</p>
+                     </div>
+
+                </div>
+            </div>
+            <div>
+                <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                    <Image 
+                    src="/cover-1.jpeg"
+                    alt= "cover-image"
+                    width={300}
+                    height={300}
+                    className="w-10 h-10 object-cover rounded-md"
+                     />
+                     <div>
+                        <p className="text-primary-text font-semibold">Midnigth Echoes</p>
+                        <p className="text-secondary-text text-sm">By Neon Skyline</p>
+                     </div>
+
+                </div>
             </div>
             
         </aside>
-        <button></button>
+        <button 
+        onClick= {() => setSidebarOpen(!sidebarOpen)}
+        className="fixed bottom-5 bg-background w-12 h-12 grid place-items-center text-white rounded-full z-50 cursor-pointer left-5 lg:hidden">
+            <MdOutlineLibraryMusic/>
+        </button>
     </div>
   )
 }
