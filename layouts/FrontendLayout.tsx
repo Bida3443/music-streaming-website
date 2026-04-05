6098da2bde3a2b4d6644e147d009b930e18ab8c1
Bida3443 <alexanderbamise@gmail.com>
2026-04-05 10:53:56 -0700

    first commit

--- layouts/FrontendLayout.tsx
+++ layouts/FrontendLayout.tsx
@@ -31,7 +31,7 @@ export default function FrontendLayout({
     }
  
     }>
-<div className="min-h-screen flex ">
+<div className="min-h-screen flex justify-center mx-auto ">
       <Navbar />
       <main>
         <Sidebar />
