48a17c66359e3c8fae1fa7bd1680547f4f335976
Bida3443 <alexanderbamise@gmail.com>
2026-04-07 05:22:28 -0700

    first commit

--- lib/SupabaseClient.ts
+++ lib/SupabaseClient.ts
@@ -1,5 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 
-const supabaseURL = ;
-const supabaseAnonKey = ;
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL! ;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase =  createClient(supabaseURL,supabaseAnonKey);
