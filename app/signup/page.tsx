6c6daa17769a3976f64e2df98cfbf05fce4d17f5
Bida3443 <alexanderbamise@gmail.com>
2026-04-09 13:50:42 -0700

    first commit

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,42 +1,73 @@
 // import React from 'react'
-"use client"
-import Image from "next/image"
-import Link from "next/link"
-import { useState } from "react"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
+export default function Page() {
+  const [Name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-export default function Page(){
-    const [Name,setName] = useState ("");
-    const [email,setEmail] = useState ("");
-    const [Password,setPassword] = useState ("");
-    const [mesage,setMessage] = useState("")
-    
+  const handleSignup = async (e : React.FormEvent) =>{
+
+    e.preventDefault();  
+
+  } 
 
   return (
     <div className="h-screen flex justify-center items-center w-full bg-hover">
-        <div className="bg-background flex flex-col items-center px-0 lg:px-12 py-6 rounded-md max-w-100 w-[90%]">
-            <Image
-            src="/logo.png"
-            alt="image" 
-            width={500}
-            height={500}
-            className="h-11 w-11"/>
-            <h2 className="text-2xl font-bold text-white mb-3">Sign up to Sportify</h2>
-            <form>
-                <input type="text" placeholder="Your Name " className="outline-none border border-neutral-600 p-2 w-full rounded-md text-primary-text placeholder-neutral-600 mb-6 focus:text-secondary-text" />
-                <input type="text" placeholder="Your Email " className="outline-none border border-neutral-600 p-2 w-full rounded-md text-primary-text placeholder-neutral-600 mb-6 focus:text-secondary-text" />
+      <div className="bg-background flex flex-col items-center px-0 lg:px-12 py-6 rounded-md max-w-100 w-[90%]">
+        <Image
+          src="/logo.png"
+          alt="image"
+          width={500}
+          height={500}
+          className="h-11 w-11"
+        />
+        <h2 className="text-2xl font-bold text-white mb-3">
+          Sign up to Sportify
+        </h2>
+        <form >
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={Name}
+            type="text"
+            placeholder="Your Name "
+            className="outline-none border border-neutral-600 p-2 w-full rounded-md text-primary-text placeholder-neutral-600 mb-6 focus:text-secondary-text"
+          />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            placeholder="Your Email "
+            className="outline-none border border-neutral-600 p-2 w-full rounded-md text-primary-text placeholder-neutral-600 mb-6 focus:text-secondary-text"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            value={Password}
+            type="text"
+            placeholder="Your Password "
+            className="outline-none border border-neutral-600 p-2 w-full rounded-md text-primary-text placeholder-neutral-600 mb-6 focus:text-secondary-text"
+          />
 
-                <input type="text" placeholder="Your Password " className="outline-none border border-neutral-600 p-2 w-full rounded-md text-primary-text placeholder-neutral-600 mb-6 focus:text-secondary-text" />
+          <button className=" w-full font-bold bg-primary text-black py-3 cursor-pointer rounded-full">
+            Continue
+          </button>
 
-                <button className=" w-full font-bold bg-primary text-black py-3 cursor-pointer rounded-full">Continue</button>
-
-                <div className="text-secondary-text text-center my-6">
-                    <span >Already have an Account?</span>
-                    <Link className="ml-2 text-white underline hover:text-primary" href="/login"> Sign in Now</Link>
-                </div>
-
-            </form>
-        </div>
+          <div className="text-secondary-text text-center my-6">
+            <span>Already have an Account?</span>
+            <Link
+              className="ml-2 text-white underline hover:text-primary"
+              href="/login"
+            >
+              {" "}
+              Sign in Now
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
